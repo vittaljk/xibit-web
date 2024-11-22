6eb1fa1c790f44aaa159cf4788a2549f50ae7618
vittaljk <vittaljk@gmail.com>
2024-11-22 21:08:20 +0530

    test commit

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,7 +3,7 @@ import * as Templates from "@/components/Templates";
 export default function Contact() {
   return (
     <Templates.Layout>
-      <div>Contact page</div>
+      <div>Contact page.</div>
     </Templates.Layout>
   );
 }
